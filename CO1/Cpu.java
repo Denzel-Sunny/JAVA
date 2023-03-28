8860c2988fca0bdc4421ed6814269531184310cd
Denzel-Sunny <125428975+Denzel-Sunny@users.noreply.github.com>
2023-03-28 15:42:55 +0530

    Add files via upload

---
+++ CO1/Cpu.java
@@ -0,0 +1,58 @@
+class Cpu {
+    int price;
+
+    Cpu(int p) {
+        this.price = p;
+    }
+
+    class Processor {
+        int cores;
+        String manufacture;
+
+        Processor(int n, String m) {
+            this.cores = n;
+            this.manufacture = m;
+        }
+
+        void display() {
+            System.out.println("No of Cores : " + this.cores);
+            System.out.println("Processor manufactures : " + this.manufacture);
+        }
+    }
+
+    static class Ram {
+        int memory;
+        String manufacture;
+
+        Ram(int n, String m) {
+            this.memory = n;
+            this.manufacture = m;
+        }
+
+        void display() {
+            System.out.println("Memory Size : " + this.memory);
+            System.out.println("Memory manufactures : " + this.manufacture);
+        }
+    }
+
+    void display() {
+        System.out.println("Price of CPU : " + this.price);
+    }
+
+    public static void main(String[] args) {
+    
+    	System.out.println("Name: Denzel Sunny");
+System.out.println("Addmission_no: 22MCA022");
+System.out.println("Course ID & Code : OOP LAB, 20MCA132");
+System.out.println("Date: 24/03/2023");
+System.out.println("____________________________________\n\n");
+
+
+        Cpu intel = new Cpu(23000);
+        Cpu.Processor i_processor = intel.new Processor(4, "intel");
+        Cpu.Ram i_ram = new Ram(1024, "Asus");
+        intel.display();
+        i_processor.display();
+        i_ram.display();
+    }
+}
