fd4563a2648477f97c16f7c68eeb6580ad54f668
Denzel-Sunny <125428975+Denzel-Sunny@users.noreply.github.com>
2023-06-27 15:01:06 +0530

    Add files via upload

---
+++ CO4/Q2/arithmetic/Addition.java
@@ -0,0 +1,7 @@
+package arithmetic;
+
+public class Addition implements Arithmetic {
+    public double calculate(double a, double b) {
+        return a + b;
+    }
+}
---
+++ CO4/Q2/arithmetic/Arithmetic.java
@@ -0,0 +1,5 @@
+package arithmetic;
+
+public interface Arithmetic {
+    double calculate(double a, double b);
+}
